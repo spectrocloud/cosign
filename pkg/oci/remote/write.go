d81053a488a774a5724a037d9c8499f1e7fc7464
Nianyu Shen <nianyu@spectrocloud.com>
2025-02-21 20:33:40 -0800

    disable progress log (#15)
    * fix: remove progress log output to stdout in remote functions
    
    * fix lint

--- pkg/oci/remote/write.go
+++ pkg/oci/remote/write.go
@@ -23,7 +23,6 @@ import (
 	"os"
 	"time"
 
-	"github.com/google/go-containerregistry/pkg/logs"
 	"github.com/google/go-containerregistry/pkg/name"
 	v1 "github.com/google/go-containerregistry/pkg/v1"
 	"github.com/google/go-containerregistry/pkg/v1/remote"
